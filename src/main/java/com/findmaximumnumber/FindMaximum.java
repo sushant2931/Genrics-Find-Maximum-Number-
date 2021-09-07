a4b5bdf817a7f3fb4684b220503834e39b823bfe
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 03:36:03 +0530

    Find Floats Maximum

--- src/main/java/com/findmaximumnumber/FindMaximum.java
+++ src/main/java/com/findmaximumnumber/FindMaximum.java
@@ -2,7 +2,7 @@ package com.findmaximumnumber;
 
 public class FindMaximum implements MaxOperation {
 	
-	    public static void main( String[] args )
+	 public static void main( String[] args )
 	    {
 	    	FindMaximum find = new FindMaximum();
 	        Integer intObj1 = Integer.valueOf("800");
@@ -10,6 +10,13 @@ public class FindMaximum implements MaxOperation {
 	        Integer intObj3 = Integer.valueOf("500");
 	        int maxIntValue = find.findMaxInt(intObj1, intObj2, intObj3);
 	        System.out.println("Integer Maximum : " + maxIntValue);
+	        
+	        Float floatObj1 = Float.valueOf("7.2");
+	        Float floatObj2 = Float.valueOf("10.2");
+	        Float floatObj3 = Float.valueOf("6.4");
+	        float maxFloatValue = find.findMaxFloat(floatObj1, floatObj2, floatObj3);
+	        System.out.println("Float Maximum : " + maxFloatValue);
+	        
 	    }
 	    public int findMaxInt(Integer intObj1, Integer intObj2, Integer intObj3) {
 	    	
@@ -27,5 +34,22 @@ public class FindMaximum implements MaxOperation {
 	    		maxIntValue = intObj3;
 	    	}
 	    	return maxIntValue;
+	    }
+	    public float findMaxFloat(Float floatObj1, Float floatObj2, Float floatObj3) {
+	    	
+	    	float maxFloatValue;
+	    	float result1 =  Float.compare(floatObj1,floatObj2);
+	    	float result2 =  Float.compare(floatObj1,floatObj3);
+	    	float result3 =  Float.compare(floatObj2,floatObj3);
+	    	if(result1 > 0 && result2 > 0) {
+	    		maxFloatValue = floatObj1;
+	    	}
+	    	else if(result3 > 0 && result1 < 0) {
+	    		maxFloatValue = floatObj2;
+	    	}
+	    	else{
+	    		maxFloatValue = floatObj3;
+	    	}
+	    	return (float)maxFloatValue;
 	    }
 }
